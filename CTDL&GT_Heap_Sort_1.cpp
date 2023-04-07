1969814a836d693d76d345c106ac02b05efebeb9
Tan-cntt5 <125740918+Tan-cntt5@users.noreply.github.com>
2023-04-07 07:52:28 +0700

    Create CTDL&GT_Heap_Sort_1.cpp

---
+++ CTDL&GT_Heap_Sort_1.cpp
@@ -0,0 +1,12 @@
+#nguon: https://vimentor.com/en/lesson/sap-xep-heap-sort
+Cai dat
+void max_heap_sort(int A[ ])
+{
+    int heap_size = N;
+    run_maxheap(A);
+    for(int i = N; i>=2 ; i-- )
+    {
+        swap(A[ 1 ], A[ i ]);
+        max_heap(A, 1, --heap_size);
+    }
+}
