b336300652dc26314b2069e5c91904e6a6b5b41a
Tan-cntt5 <125740918+Tan-cntt5@users.noreply.github.com>
2023-04-07 07:30:49 +0700

    Create CTDL&GT_Selection_Sort_1.cpp

---
+++ CTDL&GT_Selection_Sort_1.cpp
@@ -0,0 +1,25 @@
+#nguon: https://freetuts.net/thuat-toan-sap-xep-chon-selection-sort-2931.html
+Cai dat:
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    // Di chuyển ranh giới của mảng đã sắp xếp và chưa sắp xếp
+    for (i = 0; i < n-1; i++)
+    {
+    // Tìm phần tử nhỏ nhất trong mảng chưa sắp xếp
+    min_idx = i;
+    for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+        min_idx = j;
+  
+    // Đổi chỗ phần tử nhỏ nhất với phần tử đầu tiên
+        swap(arr[min_idx], arr[i]);
+    }
+}
+*Ham Swap()
+  void swap(int &xp, int &yp)
+{
+    int temp = xp;
+    xp = yp;
+    yp = temp;
+}
